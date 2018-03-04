1dfe7403724b8568b4547843c2d7ff53b95ddd76
LiangCk-h <liang.ck92@hotmail.com>
2018-03-04 15:07:54 +0800

    +sequelizer; +eslint

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
+  message: 'Welcome to the beginning of nothingness.'
 }));
 
 module.exports = app;
